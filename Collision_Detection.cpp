23ea12bec94d29979731528ef5aa9a675c23ed89
Edward Cui <37892357+ECuiDev@users.noreply.github.com>
2023-11-04 13:54:54 -0700

    Add project files.

---
+++ Collision_Detection.cpp
@@ -0,0 +1,12 @@
+﻿// Collision_Detection.cpp : Defines the entry point for the application.
+//
+
+#include "Collision_Detection.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ Collision_Detection.h
@@ -0,0 +1,8 @@
+﻿// Collision_Detection.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
